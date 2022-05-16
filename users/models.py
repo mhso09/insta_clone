09e745c2569cfeb2e211f21ead2d8fafa10e7bd6
mhso09 <mhso0@naver.com>
2022-05-16 17:46:29 +0900

    db 연동

---
+++ users/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
+
+# Create your models here.
+class User(AbstractUser):
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('C', 'Custom'),
+    ]
+
+    name = models.CharField(_('Name of User'), max_length=255, blank=True)
+    user_name = models.CharField(blank=True, max_length=255)
+    profile_photo = models.ImageField(blank=True)
+    website = models.URLField(blank=True)
+    bio = models.TextField(blank=True)
+    email = models.CharField(blank=True, max_length=255)
+    phone_number = models.CharField(blank=True, max_length=255)
+    gender = models.CharField(blank=True, max_length=255, choices=GENDER_CHOICES)
+    followers = models.ManyToManyField('self')
+    following = models.ManyToManyField('self')
---
+++ users/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url, include
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
